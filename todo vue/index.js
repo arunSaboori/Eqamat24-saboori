b4f3de687f0033de3316b0508b9057ca95cee2e5
arun <arun.saboori@gmail.com>
2022-08-24 19:53:13 +0430

    deleted somefolder

--- todo vue/index.js
+++ todo vue/index.js
@@ -8,33 +8,38 @@ createApp({
             footer: "all content blong to saboori 2022 todolist app",
             title: "title",
             date: "time",
-            showcreate: false,
             table: true,
+            show: true,
+            show2: false,
             newItemone: '',
             newItemto: '',
             tits: [],
-            dats:[],
-            Currents:[],
-      
-            }
+            dats: [],
+            Currents: [],
+
+        }
     },
     methods: {
 
-          add(){
+        add() {
             const current = new Date();
-            const date = current.getFullYear()+'-'+(current.getMonth()+1)+'-'+current.getDate();
+            const date = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
             const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
-            const dateTime = date +' '+ time;
+            const dateTime = date + ' ' + time;
 
             this.tits.push(this.newItemone);
-            this.newItemone='';
+            this.newItemone = '';
             this.dats.push(this.newItemto);
-            this.newItemto='';
+            this.newItemto = '';
             this.Currents.push(dateTime);
-             
-             
-             },
-      }
+
+        },
+        change() {
+            this.show = !this.show;
+            this.show2 = !this.show2;
+        },
+
+    }
 
 
 }).mount('#app')
