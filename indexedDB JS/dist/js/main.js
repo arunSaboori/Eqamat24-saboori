9880cfa5daa541e700ace602eb1643b466aae88f
Arun <arun.saboori@gmail.com>
2022-09-15 23:07:34 +0430

    fixing and addig good quility

--- indexedDB JS/dist/js/main.js
+++ indexedDB JS/dist/js/main.js
@@ -1,46 +1,72 @@
-const Submit = document.getElementById('BtnSubmit');
-const arrays = []
+//     let EnterTime=document.querySelector('#enter').value;
+//     let ExitTime =document.querySelector('#exit').value;
 
+//      let time;
+//     if (window.localStorage.getItem('Enter')==''){
+    //     time =[];
+    //     }else if(window.localStorage.getItem('Exit')==''){
+        //      time =[];
+        //     }else{
+            //         JSON.parse.localStorage.getItem('Enter');
+            //         JSON.parse.localStorage.getItem('Exit');
+            //     }
+            //    time.push(EnterTime);
+            //    time.push(ExitTime);
+            const tbody = document.querySelector('.tbody');
 
-// for (let i = 0; i <=1000; i++) {
+            let ShowEnterArray=[];
+            let ShowExitArray=[];
+            
+            ShowEnterArray.push(JSON.parse(window.localStorage.getItem('Enter')))
+            ShowExitArray.push(JSON.parse(window.localStorage.getItem('Exit')))
+             
+document.getElementById('BtnSubmit').addEventListener('click',function btn(){
+    let EnterTimeArray=[];
+    let ExitTimeArray=[];
 
-//     addItmeTbody();
-//     };
-
-
- function addItmeTbody(enter , exit){
-    const display =document.querySelector('tbody');
-    let row = document.createElement("tr");
+    let EnterTime=document.querySelector('#enter').value;
+    let ExitTime =document.querySelector('#exit').value;
     
-    let enterEN = document.createElement("td");
-    let enterEX = document.createElement("td");
-    let action = document.createElement("td");
-    var GetEnter =localStorage.getItem('enterTime');
-    var GetExit  =localStorage.getItem('exitTime');
-    
-    enterEN.innerText=GetEnter;
-    enterEX.innerText=GetExit;
-    action.innerText="delete";
-    row.appendChild(enterEN);
-    row.appendChild(enterEX);
-    display.insertBefore(row , display.childNodes[0]);
-}
+    EnterTimeArray.push(EnterTime);
+    ExitTimeArray.push(ExitTime);
 
+    if(EnterTime==''&& ExitTime==''){
+         alert('both inputs are emty');
+    }else if(ExitTime==''){ 
+       alert('Exit time is emty') 
+    }else if(EnterTime==''){
+        alert('Enter time are emty')
+    }    
 
+   
+   window.localStorage.setItem('Enter',JSON.stringify.appendChild(EnterTimeArray)); 
+   window.localStorage.setItem('Exit',JSON.stringify.appendChild(ExitTimeArray)); 
 
-
-Submit.addEventListener("click",e=>{
-    const enter_time = document.getElementById('enter').value;
-    const exit_time = document.getElementById('exit').value;
-    addItmeTbody(enter_time,exit_time)
-
-    localStorage.setItem("enterTime",enter_time);
-    localStorage.setItem("exitTime",exit_time);
-
-
-    });
-
-// display.innerHTML=`${GetEnter}+" and your exit time is" +${GetExit}`;
-
-
+   
   
+
+})   
+
+
+// let ShowEnter=JSON.parse(window.localStorage.getItem('Enter'));
+// let ShowExit =JSON.parse(window.localStorage.getItem('Exit'));
+const  td1 = document.createElement('td');
+const  td2 = document.createElement('td');
+const   i  = document.createElement('i');
+
+i.className = ' fa-solid   fa-trash icon';
+i.style='color:blue;'
+
+const LocalStorageTime1 = document.createTextNode(ShowEnterArray);
+const LocalStorageTime2 = document.createTextNode(ShowExitArray);
+td1.appendChild(LocalStorageTime1);
+td2.appendChild(LocalStorageTime2);
+td2.appendChild(i);
+tbody.appendChild(td1);
+tbody.appendChild(td2);
+
+
+let icon = document.querySelector('.icon').addEventListener('click',function(){
+    window.localStorage.removeItem('Enter');
+    window.localStorage.removeItem('Exit');
+ });
